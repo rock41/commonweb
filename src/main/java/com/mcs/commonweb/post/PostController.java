30a815e006658ed3a8c61a94064e5365c56600cb
rock41 <sting41@gmail.com>
2021-03-21 01:30:25 +0900

    Domain Class Converter Sample Codes

---
+++ src/main/java/com/mcs/commonweb/post/PostController.java
@@ -0,0 +1,24 @@
+package com.mcs.commonweb.post;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+public class PostController {
+
+    private final PostRepository postRepository;
+
+    public PostController(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    @GetMapping("/posts/{id}")
+    public String getPost(@PathVariable Long id) {
+        Optional<Post> byId = postRepository.findById(id);
+        Post post = byId.get();
+        return post.getTitle();
+    }
+}
