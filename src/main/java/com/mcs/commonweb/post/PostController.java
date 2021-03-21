ca81626613fccc464c7e3477186d5762477fd0ab
rock41 <sting41@gmail.com>
2021-03-21 23:06:10 +0900

    Hateoas 샘플코드 추가

--- src/main/java/com/mcs/commonweb/post/PostController.java
+++ src/main/java/com/mcs/commonweb/post/PostController.java
@@ -1,7 +1,9 @@
 package com.mcs.commonweb.post;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,8 +22,10 @@ public class PostController {
         return post.getTitle();
     }
 
+    // PagedModel 로 변경되어 사용가능
+    // Hateoas 사용
     @GetMapping("/posts")
-    public Page<Post> getPosts(Pageable pageable) {
-        return postRepository.findAll(pageable);
+    public PagedModel<EntityModel<Post>> getPosts(Pageable pageable, PagedResourcesAssembler<Post> assembler) {
+        return assembler.toModel(postRepository.findAll(pageable));
     }
 }
